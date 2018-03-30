c6239c4f560595aae29a4fa5a8d439befe694040
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-03-30 11:11:55 -0700

    minor tweak to scinet script.

--- scripts/run_scinet_test.sh
+++ scripts/run_scinet_test.sh
@@ -8,6 +8,8 @@ module load anaconda3
 source activate blackhole_fitting
 module load intel/15.0.2 openmpi/intel/1.6.4
 
+cd $PBS_O_WORKDIR
+
 WORKDIR=/home/b/bmg/aaronew/global_bh/
 
 mpirun -np 10 python \
