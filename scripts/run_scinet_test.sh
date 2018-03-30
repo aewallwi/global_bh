69302520f1e14f74189cc6c033430c68d4a3cafc
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-03-29 21:37:49 -0700

    modify scinet script.

--- scripts/run_scinet_test.sh
+++ scripts/run_scinet_test.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # MOAB/Torque submission script for SciNet GPC
 #
-#PBS -l nodes=2:ppn=8,walltime=1:00:00
+#PBS -l nodes=2:ppn=5,walltime=1:00:00
 #PBS -N test
-
-module load intel/15.0.2 openmpi/intel/1.6.4
+# load modules (must match modules used for compilation)
 module load anaconda3
-
 source activate blackhole_fitting
+module load intel/15.0.2 openmpi/intel/1.6.4
 
-cd $PBS_O_WORKDIR
+WORKDIR=/home/b/bmg/aaronew/global_bh/
 
 mpirun -np 10 python \
-$PBS_O_WORKDIR/src/global_signal_black_holes_mcmc.py -c \
-$PBS_O_WORDIR/config/analytic_test_mpi.yaml
+$WORKDIR/src/global_signal_black_holes_mcmc.py -c \
+$WORKDIR/config/analytic_test_mpi.yaml
