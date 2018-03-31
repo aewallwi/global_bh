7194566e0a411ae5d68e8666347703f58936b041
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-03-31 13:14:48 -0700

    added functionality for modeling and fitting foregrounds simultaneously

--- src/global_signal_black_holes_mcmc.py
+++ src/global_signal_black_holes_mcmc.py
@@ -10,6 +10,7 @@ import yaml, emcee, argparse, yaml
 from settings import F21
 import global_signal_black_holes as GSBH
 import copy,sys,os
+import scipy.optimize as op
 
 
 def delta_Tb_analytic(freq,**kwargs):
@@ -45,6 +46,31 @@ def var_resid(resid_array,window_length=20):
     return signal.fftconvolve(window,
     np.abs(resid_array-np.mean(resid_array))**2.,mode='same')
 
+def Tbfg(x,params_dict):
+    y_model=params_dict['APOLY0']*(x/x[int(len(x)/2)])**(-2.5)
+    for n in range(1,int(params_dict['NPOLY'])):
+        y_model+=params_dict['APOLY'+str(n)]*(x/x[int(len(x)/2)])**(n-2.5)
+    return y_model
+
+def Tb21(x,params_dict,analytic):
+    if not analytic:
+        signal_model=GSBH.delta_Tb(params_dict['ZLOW'],params_dict['ZHIGH'],
+        params_dict['NTIMES_TB'],params_dict['T4_HII'],verbose=False,
+        diagnostic=True,**params_dict)
+        x_model=F21/(signal_model['Z']+1.)/1e6
+        y_model=interp.interp1d(x_model,signal_model['Tb'])(x)
+    else:
+        y_model=delta_Tb_analytic(x,**params_dict)
+    return y_model
+
+def TbSky(params,x,params_dict,param_list,analytic):
+    param_instance=copy.deepcopy(params_dict)
+    for param,param_key in zip(params,param_list):
+        param_instance[param_key]=param
+    y_model=Tb21(x,param_instance,analytic)\
+    +Tbfg(x,param_instance)
+    return y_model
+
 def lnlike(params,x,y,yvar,param_template,param_list,analytic):
     '''
     log-likelihood of parameters
@@ -54,19 +80,8 @@ def lnlike(params,x,y,yvar,param_template,param_list,analytic):
         y, measured dTb
         yvar, measured error bars
     '''
-    param_instance=copy.deepcopy(param_template)
-    for param,param_key in zip(params,param_list):
-        param_instance[param_key]=param
     #run heating
-    if not analytic:
-        signal_model=GSBH.delta_Tb(param_instance['ZLOW'],param_instance['ZHIGH'],
-        param_instance['NTIMES_TB'],param_instance['T4_HII'],verbose=False,
-        diagnostic=True,**param_instance)
-        x_model=F21/(signal_model['Z']+1.)/1e6
-        y_model=interp.interp1d(x_model,signal_model['Tb'])(x)
-    else:
-        y_model=delta_Tb_analytic(x,**param_instance)
-        #interpolate model to measured frequencies
+    y_model=TbSky(params,x,param_template,param_list,analytic)
     return -np.sum(0.5*(y_model-y)**2./yvar)
 
 #Construct a prior for each parameter.
@@ -106,7 +121,8 @@ class Sampler():
     '''
     Class for running MCMC and storing output.
     '''
-    def __init__(self,config_file,verbose=False,analytic=True):
+    def __init__(self,config_file,verbose=False,analytic=True,
+    fmin=None,fmax=None):
         '''
         Initialize the sampler.
         Args:
@@ -114,6 +130,9 @@ class Sampler():
         '''
         self.verbose=verbose
         self.analytic=analytic
+        self.minimized=False
+        self.fmin=fmin
+        self.fmax=fmax
         with open(config_file, 'r') as ymlfile:
             self.config= yaml.load(ymlfile)
         ymlfile.close()
@@ -126,6 +145,12 @@ class Sampler():
             skiprows=1,delimiter=',')
         elif self.config['DATAFILE'][-3:]=='npy':
             self.data=np.load(self.config['DATAFILE'])
+        if not fmin is None:
+            select=self.data[:,0]>=fmin
+            self.data=self.data[select,:]
+        if not fmax is None:
+            select=self.data[:,0]<=fmax
+            self.data=self.data[select,:]
         self.freqs,self.tb_meas,self.dtb\
         =self.data[:,0],self.data[:,1],self.data[:,2]
         self.var_tb=var_resid(self.dtb,
@@ -135,32 +160,62 @@ class Sampler():
         self.params_all=self.config['PARAMS']
         self.params_vary=self.config['PARAMS2VARY']
         self.params_vary_priors=self.config['PRIORS']
+        self.resid=np.zeros_like(self.var_tb)
+        self.model=np.zeros_like(self.resid)
+
+    def gradient_descent(self,param_list=None):
+        '''
+        perform gradient descent on parameters specified in param_list
+        Args:
+            param_list, list of parameter names to perform gradient
+            descent on while holding all other parameters fixed.
+            update the parameters in config_all.
+        '''
+        if param_list is None:
+            param_list=self.params_vary
+        nll = lambda *args: -lnlike(*args)
+        result = op.minimize(nll,
+        [self.params_all[pname] for pname in param_list],
+        args=(self.freqs,self.tb_meas,self.var_tb,
+        self.params_all,param_list,self.analytic))["x"]
+        for pnum,pname in enumerate(param_list):
+            self.params_all[pname]=result[pnum]
+        self.model=TbSky(result,self.freqs,self.params_all,[],self.analytic)
+        self.resid=self.tb_meas-self.model
+
+    def approximate_ml(self):
+        if self.params_all['NPOLY']>0:
+            params_nofg=[]
+            params_fg=[]
+            for pname in self.params_vary:
+                if 'APOLY' not in pname:
+                    params_nofg=params_nofg+[pname]
+                else:
+                    params_fg=params_fg+[pname]
+            #perform gradient descent on foregrounds first
+            self.gradient_descent(params_fg)
+            #perform gradient descent on no-fg params
+            self.gradient_descent(params_nofg)
+            #perform gradient descent on all params
+        self.gradient_descent()
+        self.minimized=True
+
 
     def sample(self):
         '''
         Run the MCMC.
         '''
+        #first make sure that the maximum likelihood params are fitted
+        if not self.minimized:
+            self.approximate_ml()
+        print(self.params_all)
         ndim,nwalkers=len(self.params_vary),self.config['NWALKERS']
-
-        #perturb initial conditions
-        #draw from prior to seed walkers.
         p0=np.zeros((nwalkers,len(self.params_vary)))
+
+        pml=[self.params_all[pname] for pname in self.params_vary]
         for pnum,pname in enumerate(self.params_vary):
-            if self.params_vary_priors[pname]['TYPE']=='UNIFORM':
-                p0[:,pnum]=np.random.rand(nwalkers)\
-                *(self.params_vary_priors[pname]['MAX']\
-                -self.params_vary_priors[pname]['MIN'])\
-                +self.params_vary_priors[pname]['MIN']
-            elif self.params_vary_priors[pname]['TYPE']=='GAUSSIAN':
-                p0[:,pnum]=np.random.randn(nwalkers)*sefl.params_vary_priors[pname]['STD']\
-                +self.params_vary_priors[pname]['MEAN']
-            elif self.params_vary_priors[pname]['TYPE']=='LOGNORMAL':
-                p0[:,pnum]=np.random.lognormal(mean=self.params_vary_priors[pname]['MEAN'],
-                sigma=self.params_vary_priors[pname]['STD'],size=nwalkers)
-            else:
-                p0[:,pnum]=(np.randn(nwalkers)*self.params_vary_priors['DEFAULT_STD']\
-                +1.)*params_all[pname]#if prior not listed, start walkers randomly
-                #distributed.
+            p0[:,pnum]=(np.random.randn(nwalkers)\
+            *self.config['SAMPLE_BALL']+1.)*pml[pnum]
         args=(self.freqs,self.tb_meas,self.var_tb,
         self.params_all,self.params_vary,
         self.params_vary_priors,self.analytic)
@@ -186,6 +241,7 @@ class Sampler():
             yaml.dump(self.config,yaml_file,default_flow_style=False)
             np.save(self.config['PROJECT_NAME']+'/chain.npy',
             self.sampler.chain)
+
 
 
 '''
