bf7ca2f9ace479b4deba271ab7a27979d9f85ac8
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-04-02 14:33:15 -0700

    another typo

--- scripts/inject_signals_scinet.py
+++ scripts/inject_signals_scinet.py
@@ -6,15 +6,15 @@
 import sys,os,glob,yaml
 signal_list=\
 glob.glob('/home/b/bmg/aaronew/global_bh/tables/signal_injections/*.npy')
-ouputdir='/home/b/bmg/aaronew/global_bh/signal_injections/'
-if not os.path.exists(outputdir):
-    os.mkdir(outputdir)
+output='/home/b/bmg/aaronew/global_bh/signal_injections/'
+if not os.path.exists(output):
+    os.mkdir(output)
 #load up template .yaml file
 config=\
 yaml.load('/home/b/bmg/aaronew/global_bh/config/injection_template_scinet.yaml')
 for signal_file in signal_list:
     config['DATA_FILE']=signal_file
-    config['PROJECT_NAME']=output_dir+signal_file[:-4]
+    config['PROJECT_NAME']=output+signal_file[:-4]
     yaml.dump(config,config['PROJECT_NAME']+'/config.yaml')
     cmd='qsub -v CONFIG=\'%s\' %s\signal_injection_scinet.sh'%(config['PROJECT_NAME'],
     '/home/b/bmg/aaronew/global_bh/scripts/')
